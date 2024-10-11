b04415bd6c03a92bac05e9135a417734dabe26cb
nachete905 <ignaciosanzmesa@gmail.com>
2024-10-11 11:24:04 +0200

    Tablas de documentacion realizadas

---
+++ aplicacion/Frontend/frontapp/src/componentes/tablaPropietarios/DocumentacionPropietario.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../tablaCoches/documentacion.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+function DocumentacionPropietario(){}
+
+export default DocumentacionPropietario;
